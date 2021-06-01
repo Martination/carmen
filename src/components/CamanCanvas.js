9c23d3ff7cfd2aa2466888b0cb9fb438e1ce844e
Martin <martingreen@outlook.com>
2021-05-31 22:44:07 -0700

    Bootstrap button experiments, using code from Carol Skelly on https://dev.to/codeply/is-bootstrap-5-react-worthy-4493

--- src/components/CamanCanvas.js
+++ src/components/CamanCanvas.js
@@ -1,6 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Toast, Button } from 'bootstrap';
+
 import FilterListItem from './FilterListItem'
 const caman = window.Caman;
+
+// const { Toast } = bootstrap;
 
 // TO DO:
 // channels {red: [-100,100], green: [-100,100], blue:[-100:100]}
@@ -17,6 +21,108 @@ const caman = window.Caman;
 // edgeEnhance, edgeDetect, emboss, posterize,
 
 
+
+function ToastDemo() {
+  var [toast, setToast] = useState(false);
+  const toastRef = useRef();
+
+  useEffect(() => {
+    var myToast = toastRef.current
+    var bsToast = Toast.getInstance(myToast)
+    // var bsToast = bootstrap.Toast.getInstance(myToast)
+
+    if (!bsToast) {
+      // initialize Toast
+      console.log("init")
+      bsToast = new Toast(myToast, { autohide: false })
+      // hide after init
+      bsToast.hide()
+      setToast(false)
+    }
+    else {
+      // toggle
+      console.log("toggle")
+      toast ? bsToast.show() : bsToast.hide()
+
+    }
+  })
+
+  return (
+    <div className="py-2">
+      <button className="btn btn-success" onClick={() => setToast(toast => !toast)}>
+        Toast {toast ? 'hide' : 'show'}
+      </button>
+      <div className="toast" role="alert" ref={toastRef}>
+        <div className="toast-header">
+          <strong className="me-auto">Bootstrap 5</strong>
+          <small>4 mins ago</small>
+          <button type="button" className="btn-close" onClick={() => setToast(false)} aria-label="Close"></button>
+        </div>
+        <div className="toast-body">
+          Hello, world! This is a toast message.
+          </div>
+      </div>
+    </div>
+  )
+}
+
+function ButtonDemo() {
+  var [button, setButton] = useState(false);
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    var myButton = buttonRef.current
+    var bsButton = Button.getInstance(myButton)
+    // var bsButton = bootstrap.Button.getInstance(myButton)
+
+    if (!bsButton) {
+      // initialize Button
+      // console.log("init")
+      bsButton = new Button(myButton, { toggle: "button" })
+      // bsButton.toggle()
+      // setButton(true)
+    }
+    else {
+      // toggle
+      // console.log("toggle")
+      bsButton.toggle();
+      // button ? bsButton.toggle() : bsButton.toggle()
+
+    }
+  })
+
+  return (
+    <div className="p-2">
+      <button className="btn btn-success" ref={buttonRef} onClick={() => setButton(button => !button)}>
+        {`Button ${button ? 'on' : 'off'}`}
+      </button>
+    </div>
+  )
+}
+
+function CreateButton() {
+  var [button, setButton] = useState(false);
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    var myButton = buttonRef.current
+    var bsButton = Button.getInstance(myButton)
+
+    if (!bsButton) { bsButton = new Button(myButton, { toggle: "button" }) }
+    else { bsButton.toggle(); }
+  }, [button])
+
+  return (
+    <div className="col p-1 d-grid">
+      <button className="btn btn-primary" ref={buttonRef} onClick={() => setButton(!button)} type="button">
+        {`Button ${button ? 'on' : 'off'}`} - Lomo as a test with long text
+          </button>
+    </div>
+  )
+}
+
+// <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+
 // window.Caman.DEBUG = true
 
 // let image = "WP.png";
@@ -24,6 +130,8 @@ let image = "142.jpg";
 let htmlCanvas = "#canvas"
 // let canvas =
 caman(htmlCanvas, image, function () {
+  // this["hazyDays"](0);
+  // this["lomo"](0);
   this.render();
 });
 
@@ -79,6 +187,14 @@ const CamanCanvas = () => {
     return filters;
   }
 
+  function createPresetList(adjustmentList, onChange) {
+    // const presets = [
+    //   "vintage", "lomo", "clarity", "sinCity", "sunrise", "crossProcess", "orangePeel",
+    //   "love", "grungy", "jarques", "pinhole", "oldBoot", "glowingSun",
+    //   "hazyDays", "herMajesty", "nostalgia", "hemingway", "concentrate"
+    // ];
+  }
+
   /* Sliders Callback */
   const updateFilters = (event, init) => {
     const filter = event.target.name;
@@ -129,17 +245,70 @@ const CamanCanvas = () => {
 
   return (
     <>
+
+
+
       <div className="edit-pane bg-gray container-lg text-center p-4 lh-1">
         <img alt="Editing Canvas" className="img-fluid" id="canvas" src={image}></img>
       </div>
 
       <div id="filterHolder">
-        <div id="filterList" className="container bg-dark text-center my-3 p-2">
+        <div id="filterList" className="container overflow-hidden bg-dark text-center my-3 p-2">
           <div className="row row-cols-1 row-cols-md-2 center">
             {createFilterList(adjustmentList, updateFilters)}
           </div>
+
+          <hr />
+
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 m-1">
+            <CreateButton />
+            <CreateButton />
+            <CreateButton />
+          </div>
+
+          <hr />
+          {/* "vintage", "lomo", "clarity", "sinCity", "sunrise", "crossProcess", "orangePeel", */}
+
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 m-1">
+            <div className="col p-1 d-grid">
+              <button className="btn btn-primary" type="button" name="hazyDays" data-bs-toggle="button"
+                onClick={(event) => console.log(event.target.name)}>Hazy Days</button>
+            </div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+            <div className="col p-1 d-grid"><button className="btn btn-primary" data-bs-toggle="button" autoComplete="off" type="button">Button</button></div>
+          </div>
+
+          <hr />
+
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6">
+            <div className="p-1"><label className="btn btn-primary" data-bs-toggle="button">Hazy Dog</label></div>
+            <div className="p-1"><label className="btn btn-primary">Lomo</label></div>
+            <div className="p-1"><label className="btn btn-primary">Button</label></div>
+            <div className="p-1"><label className="btn btn-primary">Button</label></div>
+            <div className="p-1"><label className="btn btn-primary">Button</label></div>
+            <div className="p-1"><label className="btn btn-primary">Button</label></div>
+            <div className="p-1"><label className="btn btn-primary">Button</label></div>
+          </div>
+
+          <div className="py-2">
+            <button className="btn btn-primary" data-bs-target="#collapseTarget" data-bs-toggle="collapse">
+              Toggle collapse
+        </button>
+            <div className="collapse" id="collapseTarget">
+              This is the toggle-able content!
         </div>
+          </div>
+
+        </div>
+
+
       </div>
+
     </>
   );
 }
