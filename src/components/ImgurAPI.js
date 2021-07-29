4639950ca89b1878c73fd98b8a9d74188a2664ac
Martin <martingreen@outlook.com>
2021-07-29 01:05:48 -0700

    Sucessful upload image using Fetch and callback

--- src/components/ImgurAPI.js
+++ src/components/ImgurAPI.js
@@ -33,22 +33,12 @@ function httpsRequest(params, postData) {
     });
 
     if (postData) {
-      // req.ContentType = 'multipart/form-data';
-      // req.AddParam('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
-      // req.AddParam('name', 'A very small image')
-      // req.setHeader('content-type', 'multipart/form-data;');
-      // req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
       req.write(postData);
     }
 
     req.end();
   });
 }
-
-// postData = {
-//   'image': 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
-//   'name': 'A very small image'
-// }
 
 export async function getImgBlob(imageUrl, imgId) {
   if (imageUrl) {
@@ -62,38 +52,18 @@ export async function getImgBlob(imageUrl, imgId) {
 
 
 // https://imgur.com/h961kWg
+// https://imgur.com/a/3AMDPNA
 
 export function getImg(imageId, setImgData) {
-
-  // const options = {
-  //   'method': 'GET',
-  //   'hostname': 'api.imgur.com',
-  //   'path': `/3/image/${imageId}`,
-  //   'headers': {
-  //     'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`
-  //   },
-  //   'maxRedirects': 20
-  // };
-
-
 
   let url = `https://api.imgur.com/3/image/${imageId}`;
 
   const options = {
     'method': 'GET',
-    // 'url': url,
-    // 'hostname': 'api.imgur.com',
-    // 'path': `/3/image/${imageId}`,
     'headers': {
       'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`,
     },
     'maxRedirects': 20,
-    // 'mode': 'cors',
-    // "Access-Control-Allow-Origin": "*",
-    // "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-    // 'credentials': 'include',
-    // 'crossDomain': true,
-    // 'body': form
   };
 
   fetch(url, options)
@@ -109,150 +79,68 @@ export function getImg(imageId, setImgData) {
 
 export function getAlbum(albumId) {
 
+  let url = `https://api.imgur.com/3/album/${albumId}`;
+
   const options = {
     'method': 'GET',
-    'hostname': 'api.imgur.com',
-    'path': `/3/album/${albumId}`,
-    'headers': {
-      'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`
-    },
-    'maxRedirects': 20
-  };
-
-  return httpsRequest(options)
-    .then((results) => { return results.data.images[0].id; })
-    .catch((err) => console.error(err));
-}
-
-/*
-R0lGODdhAQABAPAAAP8AAAAAACwAAAAAAQABAAACAkQBADs
-*/
-
-export function uploadImg(img) {
-  img.data = img.data.toString().replace(/data:.+?,/, "")
-  // console.log(img.data);
-
-  // req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
-
-
-  let postData = {
-    'image': img.data,
-    'type': 'base64'//,   // Can be [file, base64, URL]
-    // 'name': img.name,
-    // 'album': albumId
-  };
-
-  // postData = `
-  // ------WebKitFormBoundary7MA4YWxkTrZu0gW
-  // Content-Disposition: form-data; name="image"
-
-  // R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7
-  // ------WebKitFormBoundary7MA4YWxkTrZu0gW--`;
-
-
-  postData = `
-------WebKitFormBoundary7MA4YWxkTrZu0gW
-Content-Disposition: form-data; name="name"
-
-A small image
-------WebKitFormBoundary7MA4YWxkTrZu0gW
-Content-Disposition: form-data; name="image"
-
-R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7
-------WebKitFormBoundary7MA4YWxkTrZu0gW--`;
-
-  // postData = {
-  //   'image': 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
-  //   'name': 'A very small image'
-  // }
-
-  let url = 'https://api.imgur.com/3/image';
-  const form = new FormData();
-  form.append('name', 'An image');
-  form.append('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
-
-  // console.log(form.getHeaders());
-  // form.submit(url, function (err, res) {
-  //   console.log(res);
-  // })
-
-  // console.log("form", postData);
-  // console.log("form", form);
-  // console.log("form", form.entries());
-
-  // form.entries().forEach((i) => console.log(i));
-
-  for (let [key, value] of form.entries()) {
-    console.log(key, value);
-  }
-
-
-  url = 'https://api.imgur.com/3/image';
-
-  const options = {
-    'method': 'POST',
-    // 'hostname': 'api.imgur.com',
-    // 'path': '/3/image',
-    // 'url': url,
     'headers': {
       'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`,
-      // 'Content-Type': 'multipart/form-data'
     },
     'maxRedirects': 20,
-    // 'mode': 'cors',
-    // "Access-Control-Allow-Origin": "*",
-    // "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-    // 'credentials': 'include',
-    // 'crossDomain': true,
+  };
+
+  return fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => { return data.data.images[0].id; })
+    .catch((err) => console.error(err));
+
+
+  // const options = {
+  //   'method': 'GET',
+  //   'hostname': 'api.imgur.com',
+  //   'path': `/3/album/${albumId}`,
+  //   'headers': {
+  //     'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`
+  //   },
+  //   'maxRedirects': 20
+  // };
+
+  // return httpsRequest(options)
+  //   .then((results) => { return results.data.images[0].id; })
+  //   .catch((err) => console.error(err));
+}
+
+
+export function uploadImg(img, callback) {
+  img.data = img.data.toString().replace(/data:.+?,/, "")
+
+  let postData = {
+    'name': img.name,
+    'image': img.data,
+    'type': 'base64'
+  };
+
+  const form = new FormData();
+  // form.append('name', 'An image');
+  // form.append('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+
+  for (let [key, value] of Object.entries(postData)) {
+    form.append(key, value);
+  }
+
+  const url = 'https://api.imgur.com/3/image';
+  const options = {
+    'method': 'POST',
+    'headers': {
+      'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`,
+    },
+    'maxRedirects': 20,
     'body': form
   };
 
   fetch(url, options)
     .then((res) => res.json())
-    .then((data) => { console.log(data.data); })
+    .then((data) => callback(data.data))
     .catch((err) => console.error(err));
 
-  // fetch(url, {
-  //   method: 'POST',
-  //   url: url,
-  //   body: form,
-  //   headers: {
-  //     'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`,
-  //     'Content-Type': 'multipart/form-data',
-  //     // 'Access-Control-Allow-Origin': '*'
-  //   },
-  // })
-  //   .then(function (response) {
-  //     //handle success
-  //     console.log(response);
-  //   })
-  //   .catch(function (response) {
-  //     //handle error
-  //     console.log(response);
-  //   });
-
-
-  // console.log(options);
-
-  // const options = {
-  //   'method': 'POST',
-  //   'hostname': 'api.imgur.com',
-  //   'path': '/3/image',
-  //   'headers': {
-  //     'Authorization': `Client-ID ${process.env.REACT_APP_CLIENTID}`,
-  //     'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
-  //   },
-  //   'maxRedirects': 20
-  // };
-
-  //   postData = `
-  // Content-Disposition: form-data; name="image"
-
-  // R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7`;
-
-  // console.log(JSON.stringify(postData))
-
-  // httpsRequest(options, form)
-  //   .then((results) => (console.log(results.data)))
-  //   .catch((err) => console.error(err));
 }
